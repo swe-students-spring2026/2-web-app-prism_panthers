4b544aadee9534e5bf255d988bdc13ce8a68fb94
Tsengelmurun Enkh-Onon <te2137@nyu.edu>
2026-02-25 02:09:22 -0500

    fixed route in the applications

--- backend/modules/applications/routes.py
+++ backend/modules/applications/routes.py
@@ -6,11 +6,11 @@ from . import service
 applications_bp = Blueprint("applications", __name__, url_prefix="/applications")
 
 
-@applications_bp.get("/new")
+@applications_bp.get("/add")
 def create_form():
     return render_template("internship/add.html")
 
-@applications_bp.post("/new")
+@applications_bp.post("/add")
 def create_submit():
     # TODO: insert application into DB
     service.create_application(
