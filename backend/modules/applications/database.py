17b811b7ecaa118413f0d64f076859f4a77f9a9a
yl11529 <yl11529@nyu.edu>
2026-02-25 15:49:12 -0500

    modified backend and frontend code for add and edit

--- backend/modules/applications/database.py
+++ backend/modules/applications/database.py
@@ -7,7 +7,7 @@ def applications_collection():
 
 def insert_application(application):
     #insert new application and return _id
-    return applications_collection().insert_one(application.todocument())
+    return applications_collection().insert_one(application.to_document())
 
 def find_application_by_id(application_id):
     #find application by _id return doc
@@ -15,8 +15,8 @@ def find_application_by_id(application_id):
 
 def update_application_by_id(application_id,updates):
     #update application with set "updates", return
-    return applications_collection().update_one({"_id":application_id},{"$set":updates})
+    return applications_collection().update_one({"_id":ObjectId(application_id)},{"$set":updates})
 
 def delete_application_by_id(application_id):
-    return applications_collection().delete_one({"_id":application_id})
+    return applications_collection().delete_one({"_id":ObjectId(application_id)})
 
